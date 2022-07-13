8dc09c970e4ce3dd88a2a8afc397139f21bb2fd3
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-14 10:15:21 +1200

    Add route to redirect all other traffic back to React App

--- server/server.js
+++ server/server.js
@@ -10,4 +10,8 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/clients', clientRoutes)
 
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 module.exports = server
