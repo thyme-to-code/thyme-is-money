ccbb5a704fb6d360c18146566b76dc04a61fe772
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-13 13:52:59 +1200

    Add clients migration

--- server/db/migrations/20220713013030_clients.js
+++ server/db/migrations/20220713013030_clients.js
@@ -2,14 +2,23 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('clients', (t) => {
+    t.increments('id').primary()
+    t.string('business_name')
+    t.string('contact_name')
+    t.string('email')
+    t.string('phone')
+    t.string('address')
+    t.float('raite')
+    t.timestamps()
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('clients')
+}
