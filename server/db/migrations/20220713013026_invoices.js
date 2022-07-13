1d81a1023323e655084e24f5c003690ba0e77863
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-13 13:51:16 +1200

    Add invoices migration

--- server/db/migrations/20220713013026_invoices.js
+++ server/db/migrations/20220713013026_invoices.js
@@ -2,14 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('invoices', (t) => {
+    t.increments('id').primary()
+    t.integer('client_id')
+    t.float('total')
+    t.datetime('date_sent')
+    t.datetime('date_paid')
+    t.float('amount_paid')
+    t.timestamps()
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('invoices')
+}
