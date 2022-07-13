ae568fca9faf8f2fb8d9d0d4269befb7e801b0f6
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-13 15:48:01 +1200

    updated render to createRoot

--- client/index.js
+++ client/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './reducers/store'
 
 import App from './components/App'
 
+const container = document.getElementById('app')
+const root = createRoot(container)
 document.addEventListener('DOMContentLoaded', () => {
-  render(
+  root.render(
     <Provider store={store}>
       <App />
-    </Provider>,
-    document.getElementById('app')
+    </Provider>
   )
 })
