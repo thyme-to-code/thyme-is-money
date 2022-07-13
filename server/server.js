049d39473e3fcd2da5d313e27ecef38619194f31
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-14 10:12:15 +1200

    Add to client routes to main server file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const path = require('path')
 
 const server = express()
 
+const clientRoutes = require('./routes/clients')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1/clients', clientRoutes)
 
 module.exports = server
