12d67d78bcff51c04f47b74928a2be38f0bc2cdf
yuricowan <103348152+yuricowan@users.noreply.github.com>
2022-07-14 14:42:11 +1200

    render business names from redux

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,29 +3,27 @@ import { useSelector, useDispatch } from 'react-redux'
 import { UnorderedList, ListItem } from '@chakra-ui/react'
 
 export function Navbar() {
-  const companyNames = [
-    'accountico',
-    'Endstart',
-    'bizgiz',
-    'Bookkeeper',
-    'accountal',
-    'zaccounting',
-  ]
 
-  const capitaliseCompanyNames = companyNames.map((name) => {
-    const firstLetter = name.charAt(0).toUpperCase()
-    const remainingName = name.substring(1)
+  const clients = useSelector((state) => state.clientList)
+
+  const capitaliseCompanyNames = clients.map((client) => {
+    const firstLetter = client.business_name.charAt(0).toUpperCase()
+    const remainingName = client.business_name.substring(1)
     return firstLetter + remainingName
   })
 
   const orderedCompanyNames = capitaliseCompanyNames.sort()
+  
+
+
+
 
   return (
     <>
       <UnorderedList>
         {orderedCompanyNames &&
           orderedCompanyNames.map((company, i) => {
-            return <ListItem key={i}>{company}</ListItem>
+            return <ListItem onClick={} key={i}>{company}</ListItem>
           })}
       </UnorderedList>
     </>
