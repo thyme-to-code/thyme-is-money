168817e03f5c01ed839978b285fd65fd74cc965b
Adam Shand <130651+adamshand@users.noreply.github.com>
2022-07-14 20:31:15 +1200

    converted taskList reducer to createAsyncThunk

--- client/components/Client.jsx
+++ client/components/Client.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getUninvoicedTasks } from '../apis/tasks'
-import { loadTasks } from '../reducers/taskList'
+import { getActiveClientTasks } from '../reducers/taskList'
 
 export function Client() {
   const dispatch = useDispatch()
-  // const { selectedClient } = useSelector((state) => state.status)
   const { selectedClient } = useSelector((state) => state.clientList)
 
   useEffect(() => {
-    getUninvoicedTasks(selectedClient.id)
-      .then((tasks) => dispatch(loadTasks(tasks)))
-      .catch((err) => {
-        throw err
-      })
+    dispatch(getActiveClientTasks(selectedClient.id))
   })
 
   return (
--- client/reducers/taskList.js
+++ client/reducers/taskList.js
@@ -1,16 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
-let initialState = [] // array of {client}
+let initialState = {
+  data: [],
+  loading: false,
+}
+
+export const getActiveClientTasks = createAsyncThunk(
+  'taskList/getActiveClientTasks',
+  async (clientId) => {
+    try {
+      const res = await fetch(
+        `/api/v1/tasks/${clientId}?status=uninvoiced`
+      ).then((data) => data.json())
+      return res
+    } catch (err) {
+      return
+    }
+  }
+)
 
 export const taskListSlice = createSlice({
   name: 'taskList',
   initialState,
-  reducers: {
-    loadTasks: (state, action) => {
-      return action.payload
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getActiveClientTasks.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(getActiveClientTasks.rejected, (state) => {
+      state.loading = false
+    })
+    builder.addCase(getActiveClientTasks.fulfilled, (state, { payload }) => {
+      state.loading = false
+      state.data = payload
+    })
   },
 })
 
-export const { loadTasks } = taskListSlice.actions
 export default taskListSlice.reducer
