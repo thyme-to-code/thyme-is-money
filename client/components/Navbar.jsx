2c0b69cef6a32223524f3e20992a7a894a39cfbe
yuricowan <103348152+yuricowan@users.noreply.github.com>
2022-07-14 12:03:35 +1200

    dummy data added, capitalise and order company names

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,9 +3,31 @@ import { useSelector, useDispatch } from 'react-redux'
 import { InvoiceList } from './InvoiceList'
 
 export function Navbar() {
+  const companyNames = [
+    'accountico',
+    'Endstart',
+    'bizgiz',
+    'Bookkeeper',
+    'accountal',
+    'zaccounting',
+  ]
+
+  const capitaliseCompanyNames = companyNames.map((name) => {
+    const firstLetter = name.charAt(0).toUpperCase()
+    const remainingName = name.substring(1)
+    return firstLetter + remainingName
+  })
+
+  const orderedCompanyNames = capitaliseCompanyNames.sort()
+
   return (
     <>
       <InvoiceList />
     </>
   )
 }
+
+// step 0 - create an array of dummy company names
+// step 1 - show all company names in navbar
+// step 2 - order all company names by ABC
+// step 3 - add bar for company names
