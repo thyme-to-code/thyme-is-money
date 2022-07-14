5c28f6bf6c01063d77a39f235c7974d631ccfdc0
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-15 09:52:34 +1200

    create task modal

--- client/components/Client.jsx
+++ client/components/Client.jsx
@@ -1,11 +1,30 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getActiveClientTasks } from '../reducers/taskList'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react'
 
 export function Client() {
   const dispatch = useDispatch()
   const { selectedClient } = useSelector((state) => state.clientList)
   const taskList = useSelector((state) => state.taskList)
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = React.useRef(null)
+  const finalRef = React.useRef(null)
 
   useEffect(() => {
     dispatch(getActiveClientTasks(selectedClient.id))
@@ -19,18 +38,54 @@ export function Client() {
         <p>{selectedClient.email}</p>
         <p>{selectedClient.phone}</p>
       </div>
-      {selectedClient.business_name && (
-        <>
-          <button>Create Task</button>
-          <button>Create Invoice</button>
-        </>
-      )}
+
       <h1>Uninvoiced Tasks</h1>
       <div className="tasks">
         {taskList?.data.map((task, i) => (
           <li key={i}>{task.description}</li>
         ))}
       </div>
+      {selectedClient.business_name && (
+        <>
+          <Button onClick={onOpen}>Create Task</Button>
+        </>
+      )}
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create Task</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Description</FormLabel>
+              <Textarea ref={initialRef} placeholder="Description" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Hours</FormLabel>
+              <Input placeholder="Hours" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="green" mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
+
+// Create modal
+// update modal to correct fields
+// update modal form using state
+// on submit take state value and push to database
