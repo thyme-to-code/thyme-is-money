694bfd35409ad7a94524c6ab98ae768f74db6d21
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-15 08:57:51 +1200

    accept incoming change

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,10 +2,5 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 export function Header() {
-<<<<<<< HEAD
-  return <h1>Thyme is Money</h1>
-}
-=======
   return <h1>🌱 Thyme is Money</h1>
 }
->>>>>>> c79f7d2f998aeef1eb6638ad8053bfeed9a96421
