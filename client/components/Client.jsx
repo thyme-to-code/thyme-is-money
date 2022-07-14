8c592597a16c46fd823b5b0e05c377593007e7af
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-14 16:25:34 +1200

    client component wip

--- client/components/Client.jsx
+++ client/components/Client.jsx
@@ -3,6 +3,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { Invoice } from './Invoice'
 
 export function Client() {
+  const selectedClient = useSelector((state) => state.selectedClient)
+  // console.log(selectedClient)
+  // useEffect(() => {
+
+  // },)
   return (
     <>
       <Invoice />
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,7 +7,6 @@ export function Navbar() {
   const dispatch = useDispatch()
   const clients = useSelector((state) => state.clientList)
 
-  console.log(clients)
   const allCompanyNames = clients.map((client) => {
     return client.business_name
   })
