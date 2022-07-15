c41c11b4cc44307a366f6cdba4bde755df57eecf
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-15 13:45:38 +1200

    First working example

--- client/components/NewClient.jsx
+++ client/components/NewClient.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Formik, Field, Form } from 'formik'
+import { Formik, FormikProps, Field, Form } from 'formik'
 import {
   useDisclosure,
   useNumberInput,
@@ -42,81 +42,54 @@ export function NewClient() {
 
   return (
     <>
-      <Formik
-        initialValues={{
-          name: 'Business',
-          contact: 'Fred',
-          phone: '234324323423',
-          email: '123@me.me',
-          address: '42 Wallaby Way',
-          rate: 50,
-        }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            actions.setSubmitting(false)
-          }, 1000)
-        }}
-      >
-        {(props) => (
-          <form onSubmit={props.handleSubmit}>
-            <Button onClick={onOpen}>New Client</Button>
-            <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="outside">
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Add new client</ModalHeader>
-                <ModalCloseButton />
+      <Button onClick={onOpen}>New Client</Button>
+      <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="outside">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add new client</ModalHeader>
+          <ModalCloseButton />
+          <Formik
+            initialValues={{
+              name: 'Business',
+              // contact: 'Fred',
+              // phone: '234324323423',
+              // email: '123@me.me',
+              // address: '42 Wallaby Way',
+              // rate: 50,
+            }}
+            onSubmit={(values) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+              }, 100)
+            }}
+          >
+            {(props) => (
+              <form onSubmit={props.handleSubmit}>
                 <ModalBody>
                   <FormControl isRequired>
                     <FormLabel htmlFor="name">Business Name</FormLabel>
-                    <Field as={Input} id="name" type="text" />
+                    <Field
+                      as={Input}
+                      name="name"
+                      id="name"
+                      type="text"
+                      variant="filled"
+                    />
                   </FormControl>
-
-                  {/* // <FormLabel htmlFor="contact">Primary Contact</FormLabel>
-                        // <Input id="contact" type="text" />
-                        // <FormLabel htmlFor="phone">Contact Phone</FormLabel>
-                        // <InputGroup>
-                        //   <InputLeftElement color="gray.400">
-                        //     +
-                        //   </InputLeftElement>
-                        //   <Input id="phone" type="tel" />
-                        // </InputGroup>
-                        // <FormLabel htmlFor="email">Email address</FormLabel>
-                        // <Input id="email" type="email" />
-                        // <FormLabel htmlFor="address">Address</FormLabel>
-                        // <Textarea
-                        //   value={values.address}
-                        //   id="address"
-                        //   placeholder="42 Wallaby Way, Sydney, Australia"
-                        //   size="sm"
-                        // />
-                        // <FormLabel htmlFor="rate">Hourly Rate</FormLabel>
-                        {/* <HStack maxW="320px"> */}
-                  {/* <InputGroup>
-                        <InputLeftElement color="gray.400">$</InputLeftElement>
-                        <Input id="rate" type="number" />
-                      </InputGroup> */}
-                  {/* <Button {...dec}>-</Button>
-                        <Button {...inc}>+</Button>
-                      </HStack> */}
                 </ModalBody>
                 <ModalFooter>
                   <Button colorScheme="gray" mr={3} onClick={onClose}>
                     Cancel
                   </Button>
-                  <Button
-                    colorScheme="green"
-                    isLoading={props.isSubmitting}
-                    type="submit"
-                  >
+                  <Button colorScheme="green" type="submit">
                     Submit
                   </Button>
                 </ModalFooter>
-              </ModalContent>
-            </Modal>
-          </form>
-        )}
-      </Formik>
+              </form>
+            )}
+          </Formik>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
