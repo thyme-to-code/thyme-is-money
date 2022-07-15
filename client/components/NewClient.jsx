c2ebd0fe739e1cfc90b7b0ddc594e1552bfe5d32
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-15 16:56:51 +1200

    refactored UpdateClient and NewClient to use ClientForm.jsx

--- client/components/NewClient.jsx
+++ client/components/NewClient.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import request from 'superagent'
-import { Formik, Field, Form } from 'formik'
+import { Formik } from 'formik'
 import {
   useDisclosure,
   Modal,
   ModalOverlay,
-  ModalBody,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalCloseButton,
   Button,
-  Input,
-  FormControl,
-  FormLabel,
-  Textarea,
 } from '@chakra-ui/react'
 
 import { setSelectedClient, getClients } from '../reducers/clientList'
+import { ClientForm } from './ClientForm'
 
 export function NewClient() {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -36,10 +31,10 @@ export function NewClient() {
           <ModalCloseButton />
           <Formik
             initialValues={{
-              business_name: 'Foo',
-              contact_name: 'Bar',
-              phone: '021 123 456',
-              email: 'eat@sherman.com',
+              business_name: '',
+              contact_name: '',
+              phone: '',
+              email: '',
               address: 'New Zealand',
               rate: 50,
             }}
@@ -55,72 +50,7 @@ export function NewClient() {
                 .catch((err) => console.log(err))
             }}
           >
-            {(props) => (
-              <Form>
-                <ModalBody>
-                  <FormControl isRequired>
-                    <FormLabel htmlFor="business_name">Business Name</FormLabel>
-                    <Field
-                      as={Input}
-                      name="business_name"
-                      id="business_name"
-                      type="text"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="contact_name">
-                      Primary Contact
-                    </FormLabel>
-                    <Field
-                      as={Input}
-                      name="contact_name"
-                      id="contact_name"
-                      type="text"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="phone">Phone</FormLabel>
-                    <Field
-                      as={Input}
-                      name="phone"
-                      id="phone"
-                      type="tel"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="email">Email</FormLabel>
-                    <Field
-                      as={Input}
-                      name="email"
-                      id="email"
-                      type="email"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="address">Address</FormLabel>
-                    <Field
-                      as={Textarea}
-                      name="address"
-                      id="address"
-                      type="text"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="rate">Hourly Rate</FormLabel>
-                    <Field
-                      as={Input}
-                      name="rate"
-                      id="reate"
-                      type="number"
-                      variant="filled"
-                    />
-                  </FormControl>
-                </ModalBody>
-                <ModalFooter>
-                  <Button colorScheme="gray" mr={3} onClick={onClose}>
-                    Cancel
-                  </Button>
-                  <Button colorScheme="green" type="submit">
-                    Create
-                  </Button>
-                </ModalFooter>
-              </Form>
-            )}
+            {(props) => <ClientForm isUpdate={false} onClose={onClose} />}
           </Formik>
         </ModalContent>
       </Modal>
--- client/components/UpdateClient.jsx
+++ client/components/UpdateClient.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import request from 'superagent'
-import { Formik, Field, Form } from 'formik'
+import { Formik } from 'formik'
 import {
   useDisclosure,
   Modal,
   ModalOverlay,
-  ModalBody,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalCloseButton,
   Button,
-  Input,
-  FormControl,
-  FormLabel,
-  Textarea,
 } from '@chakra-ui/react'
 
 import { setSelectedClient, getClients } from '../reducers/clientList'
+import { ClientForm } from './ClientForm'
 
 export function UpdateClient() {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -49,72 +44,7 @@ export function UpdateClient() {
                 .catch((err) => console.log(err))
             }}
           >
-            {() => (
-              <Form>
-                <ModalBody>
-                  <FormControl isRequired>
-                    <FormLabel htmlFor="business_name">Business Name</FormLabel>
-                    <Field
-                      as={Input}
-                      name="business_name"
-                      id="business_name"
-                      type="text"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="contact_name">
-                      Primary Contact
-                    </FormLabel>
-                    <Field
-                      as={Input}
-                      name="contact_name"
-                      id="contact_name"
-                      type="text"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="phone">Phone</FormLabel>
-                    <Field
-                      as={Input}
-                      name="phone"
-                      id="phone"
-                      type="tel"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="email">Email</FormLabel>
-                    <Field
-                      as={Input}
-                      name="email"
-                      id="email"
-                      type="email"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="address">Address</FormLabel>
-                    <Field
-                      as={Textarea}
-                      name="address"
-                      id="address"
-                      type="text"
-                      variant="filled"
-                    />
-                    <FormLabel htmlFor="rate">Hourly Rate</FormLabel>
-                    <Field
-                      as={Input}
-                      name="rate"
-                      id="reate"
-                      type="number"
-                      variant="filled"
-                    />
-                  </FormControl>
-                </ModalBody>
-                <ModalFooter>
-                  <Button colorScheme="gray" mr={3} onClick={onClose}>
-                    Cancel
-                  </Button>
-                  <Button colorScheme="orange" type="submit">
-                    Update
-                  </Button>
-                </ModalFooter>
-              </Form>
-            )}
+            {() => <ClientForm isUpdate={true} onClose={onClose} />}
           </Formik>
         </ModalContent>
       </Modal>
