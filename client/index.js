b7c85c1b2a005ec40f7cbbadf8d329037d229402
yuricowan <103348152+yuricowan@users.noreply.github.com>
2022-07-18 09:57:04 +1200

    create Auth0 account for project

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './reducers/store'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 import App from './components/App'
 
@@ -9,8 +10,10 @@ const container = document.getElementById('app')
 const root = createRoot(container)
 document.addEventListener('DOMContentLoaded', () => {
   root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider domain='thyme-is-money.au.auth0.com' clientId='vrd0Ykva2cFUzMkyLc10F2Ns1vu1XoDU' audience='https://thyme-is-money/api'>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   )
 })
