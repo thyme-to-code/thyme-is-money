c43738b67fa1009b601e51e8eed16cd522341ed5
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-19 16:17:55 +1200

    save merge resolution

--- client/components/invoices/NewInvoice.jsx
+++ client/components/invoices/NewInvoice.jsx
@@ -89,11 +89,7 @@ export function NewInvoice() {
               Close
             </Button>
             {isApproved ? (
-<<<<<<< HEAD
               <Button onClick={handleClick} type="submit" bg="brand.100" color="brand.50" _hover={{ bg: "brand.200" }}>
-=======
-              <Button onClick={handleClick} type="submit" colorScheme="blue">
->>>>>>> a4e5abbdede78687ac077f4412a55e1cf702c28f
                 Download
               </Button>
             ) : (
