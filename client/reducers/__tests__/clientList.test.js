cd4ff5f9d13d2c684a07eb8b7e97f762c06480f9
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-20 09:25:22 +1200

    test clientList reducers

---
+++ client/reducers/__tests__/clientList.test.js
@@ -0,0 +1,23 @@
+import clientList, {
+  setSelectedClient,
+  clearSelectedClient,
+} from '../clientList'
+
+describe('clientList reducer', () => {
+  it('sets the selected client into state', () => {
+    const oldState = {}
+    const action = setSelectedClient({
+      business_name: 'Mickey Mouse Clubhouse',
+    })
+    const newState = clientList(oldState, action)
+    expect(newState.selectedClient).toEqual(action.payload)
+  })
+  it('clears the selected client', () => {
+    const oldState = {
+      business_name: 'Mickey Mouse Clubhouse',
+    }
+    const action = clearSelectedClient(oldState)
+    const newState = clientList(oldState, action)
+    expect(newState.selectedClient).toEqual({})
+  })
+})
