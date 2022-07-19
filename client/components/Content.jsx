d82e0d4118ce4375e2d364be0d93473eea54b639
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-19 21:06:15 +1200

    check point

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {
-  Stat,
-  StatLabel,
-  StatNumber,
-  StatHelpText,
+  Box,
+  Center,
+  CircularProgress,
+  Container,
   Divider,
   Flex,
+  Heading,
   Spacer,
-  CircularProgress,
-  Center,
+  Stat,
+  StatGroup,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
 } from '@chakra-ui/react'
 
 import { ClientDetails } from './clients/ClientDetails'
@@ -35,27 +39,96 @@ export function Content() {
     )
   }
 
-  return (
-    selectedClient.id && (
-      <>
-        <Flex>
-          <ClientDetails />
-          <Spacer />
-          {/* TODO Consider refactoring into a ClientStats component */}
-          <Stat>
-            <StatLabel>Uninvoiced Amount</StatLabel>
-            <StatNumber>${uninvoiced.amount}</StatNumber>
-            <StatHelpText>Total Hours: {uninvoiced.hours}</StatHelpText>
-          </Stat>
-        </Flex>
+  return selectedClient.id ? (
+    <>
+      <Flex>
+        <ClientDetails />
+        <Spacer />
+        {/* TODO Consider refactoring into a ClientStats component */}
+        <Stat>
+          <StatLabel>Uninvoiced Amount</StatLabel>
+          <StatNumber>${uninvoiced.amount}</StatNumber>
+          <StatHelpText>Total Hours: {uninvoiced.hours}</StatHelpText>
+        </Stat>
+      </Flex>
 
-        <Divider />
+      <Divider />
 
-        <Tasks />
+      <Tasks />
 
-        <NewTask />
-        <NewInvoice />
-      </>
-    )
+      <NewTask />
+      <NewInvoice />
+    </>
+  ) : (
+    <>
+      <Heading size="2xl">Current Summary</Heading>
+      <Flex mt="5" mr="10">
+        <Box
+          p="3"
+          borderColor="goldenrod"
+          borderWidth="2px"
+          borderRadius="lg"
+          color="goldenrod"
+          alignItems="right"
+        >
+          <Box
+            color="gray.600"
+            fontWeight="bold"
+            letterSpacing="wide"
+            fontSize="2xl"
+            textTransform="uppercase"
+          >
+            Earnings
+          </Box>
+          <Heading size="xl">$ 61,345.87</Heading>
+        </Box>
+        <Spacer />
+        <Box
+          p="3"
+          borderColor="red"
+          borderWidth="2px"
+          borderRadius="lg"
+          color="red"
+          alignItems="right"
+        >
+          <Box
+            color="gray.600"
+            fontWeight="bold"
+            letterSpacing="wide"
+            fontSize="2xl"
+            textTransform="uppercase"
+          >
+            GST
+          </Box>
+          <Heading size="xl">$ 5,344.23</Heading>
+        </Box>
+        <Spacer />
+        <Box
+          p="3"
+          borderColor="green"
+          borderWidth="2px"
+          borderRadius="lg"
+          color="green"
+          alignItems="right"
+        >
+          <Box
+            color="gray.600"
+            fontWeight="bold"
+            letterSpacing="wide"
+            fontSize="2xl"
+            textTransform="uppercase"
+          >
+            Uninvoiced
+          </Box>
+          <Heading size="xl">$ 2,003.87</Heading>
+        </Box>
+      </Flex>
+      <Heading mt="5" size="xl">
+        Uninvoiced Tasks
+      </Heading>
+      <Heading mt="5" size="xl">
+        Recent Invoices
+      </Heading>
+    </>
   )
 }
