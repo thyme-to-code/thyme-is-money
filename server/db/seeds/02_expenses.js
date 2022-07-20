594cdb03cd4c8e3ee09f6cf515a18d9ffe05eab3
Adam Shand <130651+adamshand@users.noreply.github.com>
2022-07-20 21:36:07 +1200

    change del() to truncate() in seed files

--- server/db/seeds/02_expenses.js
+++ server/db/seeds/02_expenses.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  await knex('expenses').del()
+  await knex('expenses').truncate()
   await knex('expenses').insert([
     {
       id: 1,
