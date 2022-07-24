09ba0c58e11c7b562f2cf6ef896f02ec6c031be7
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-24 14:57:53 +1200

    Update db migrations for new structure

--- server/db/migrations/02_invoices.js
+++ server/db/migrations/02_invoices.js
@@ -5,11 +5,13 @@
 exports.up = function (knex) {
   return knex.schema.createTable('invoices', (t) => {
     t.increments('id').primary()
+    t.int('user_id').references('users.id')
     t.integer('client_id').references('clients.id')
     t.float('total')
     t.datetime('date_sent')
     t.datetime('date_paid')
     t.float('amount_paid')
+    t.text('invoice_json')
     t.timestamps()
   })
 }
