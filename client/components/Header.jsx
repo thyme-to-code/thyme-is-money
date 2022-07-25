b4efef03812fcc609117c9ce957af270344eb31b
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-25 16:04:45 +1200

    create drawer menu

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,19 +3,29 @@ import { useDispatch } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react'
 import {
   Box,
-  Button,
+  Button, 
+  IconButton,
   Flex,
   Heading,
   HStack,
   Image,
   Spacer,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure
 } from '@chakra-ui/react'
+import { GoGrabber } from 'react-icons/go'
 
 import { clearSelectedClient } from '../reducers/clientList'
 
 export function Header() {
   const { logout } = useAuth0()
   const dispatch = useDispatch()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   function handleLogOut(event) {
     event.preventDefault()
@@ -27,28 +37,51 @@ export function Header() {
     dispatch(clearSelectedClient())
   }
 
+  const expandMenu = () => {
+    onOpen()
+  }
+
   return (
-    <Flex>
-      <Box>
-        <HStack>
-          <Image
-            onClick={handleClick}
-            boxSize="64px"
-            alt="Two leaves as logo"
-            src="/favicon-crop.png"
+    <>
+      <Flex>
+        <Box>
+          <HStack>
+            <Image
+              onClick={handleClick}
+              boxSize="64px"
+              alt="Two leaves as logo"
+              src="/favicon-crop.png"
+            />
+            <Spacer px="1" />
+            <Heading onClick={handleClick} as="h1" color="#fff">
+              Thyme is Money
+            </Heading>
+          </HStack>
+        </Box>
+        <Spacer />
+        <Box mr={3}>
+          <IconButton
+            onClick={expandMenu}
+            colorScheme="whiteAlpha"
+            icon={<GoGrabber />}
           />
-          <Spacer px="1" />
-          <Heading onClick={handleClick} as="h1" color="#fff">
-            Thyme is Money
-          </Heading>
-        </HStack>
-      </Box>
-      <Spacer />
-      <Box mr={3}>
-        <Button colorScheme="whiteAlpha" onClick={handleLogOut}>
-          Log Out
-        </Button>
-      </Box>
-    </Flex>
+        </Box>
+      </Flex>
+
+      <Drawer onClose={onClose} isOpen={isOpen} size={{ base: 'full', md: 'xs', lg: 'xs' }}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>{`Menu`}</DrawerHeader>
+          <DrawerBody>
+            <Button variant="outline" onClick={handleLogOut}>
+              Log Out
+            </Button>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+
+     
   )
 }
