94d69f6806c555650a1e4c8b99a4c0c77e745aff
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-26 17:18:06 +1200

    Removed old clients reducer

--- client/reducers/store.js
+++ client/reducers/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import clientList from './clientList'
 import clients from './clients'
 import taskList from './taskList'
 import invoiceList from './invoiceList'
@@ -8,7 +7,6 @@ import user from './user'
 
 export const store = configureStore({
   reducer: {
-    clientList,
     clients,
     taskList,
     user,
