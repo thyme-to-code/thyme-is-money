03c2b393390669372bb613ad6fa0028d626fac6e
David Benton <30560845+dajabe@users.noreply.github.com>
2022-08-02 11:31:59 +1200

    remove old invoiceList slice from state

--- client/reducers/store.js
+++ client/reducers/store.js
@@ -3,7 +3,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import clients from './clients'
 import taskList from './taskList'
 import tasks from './tasks'
-import invoiceList from './invoiceList'
 import invoices from './invoices'
 import user from './user'
 
@@ -13,7 +12,6 @@ export const store = configureStore({
     taskList,
     tasks,
     user,
-    invoiceList,
     invoices,
   },
 })
