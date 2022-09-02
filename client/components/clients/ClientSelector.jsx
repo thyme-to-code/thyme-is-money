21a5b04fc628f34b67d8ec63d5770d067cef2464
Adam Shand <130651+adamshand@users.noreply.github.com>
2022-09-03 07:48:42 +1200

    change client name to styled dropdown

--- client/components/clients/ClientSelector.jsx
+++ client/components/clients/ClientSelector.jsx
@@ -1,54 +1,49 @@
 // @ts-check
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Heading, Select, FormLabel } from '@chakra-ui/react'
-import { setSelectedClient, clearSelectedClient } from '../../reducers/clients'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Heading, Select, FormLabel } from "@chakra-ui/react";
+import { setSelectedClient, clearSelectedClient } from "../../reducers/clients";
 
 export function ClientSelector() {
-  const dispatch = useDispatch()
-  const clients = useSelector((state) => state.clients)
+  const dispatch = useDispatch();
+  const clients = useSelector((state) => state.clients);
 
   const orderedCompanyNames = clients.active
     .map((client) => client.business_name)
-    .sort()
+    .sort();
 
   function handleChange(e) {
     if (e.target.value) {
       const myClient = clients.active.find(
         (client) => client.business_name == e.target.value
-      )
-      return dispatch(setSelectedClient(myClient))
+      );
+      return dispatch(setSelectedClient(myClient));
     } else {
-      return dispatch(clearSelectedClient())
+      return dispatch(clearSelectedClient());
     }
   }
 
   if (clients.loading) {
-    return <>Loading ...</>
+    return <>Loading ...</>;
   }
 
   return (
-    <>
-      <FormLabel>
-        <Heading as="h2" fontSize="2xl" color="brand.100">
-          Select Client
-        </Heading>
-      </FormLabel>
-      <Select
-        onChange={handleChange}
-        size="lg"
-        width={'70%'}
-        bg="brand.300"
-        variant="outline"
-        placeholder="Summary"
-        mb="3"
-      >
-        {orderedCompanyNames.map((company, i) => (
-          <option key={i} value={company}>
-            {company}
-          </option>
-        ))}
-      </Select>
-    </>
-  )
+    <Select
+      onChange={handleChange}
+      size="lg"
+      placeholder="Select client"
+      mb="3"
+      borderColor="brand.100"
+      bg="brand.100"
+      color="brand.50"
+      _hover={{ bg: "brand.200" }}
+      style={{ fontSize: "1.4em", fontWeight: "600" }}
+    >
+      {orderedCompanyNames.map((company, i) => (
+        <option key={i} value={company}>
+          {company}
+        </option>
+      ))}
+    </Select>
+  );
 }
