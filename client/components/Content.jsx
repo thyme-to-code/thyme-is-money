e2a9593940827a8a0a853cbfdec1086ad586747a
Adam Shand <130651+adamshand@users.noreply.github.com>
2022-09-03 08:07:58 +1200

    playing with butons

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -1,48 +1,48 @@
 // @ts-check
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
 import {
   Center,
   CircularProgress,
   Divider,
   Flex,
   Spacer,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
-import { ClientDetails } from './clients/ClientDetails'
-import { NewInvoice } from './invoices/NewInvoice'
-import { NewTask } from './tasks/NewTask'
-import { Tasks } from './tasks/Tasks'
-import { UpdateClient } from './clients/UpdateClient'
-import { Overview } from './Overview'
-import { ClientInvoiceList } from './invoices/ClientInvoiceList'
+import { ClientDetails } from "./clients/ClientDetails";
+import { NewInvoice } from "./invoices/NewInvoice";
+import { NewTask } from "./tasks/NewTask";
+import { Tasks } from "./tasks/Tasks";
+import { UpdateClient } from "./clients/UpdateClient";
+import { Overview } from "./Overview";
+import { ClientInvoiceList } from "./invoices/ClientInvoiceList";
 
 export function Content() {
-  const { selected, loading } = useSelector((state) => state.clients)
+  const { selected, loading } = useSelector((state) => state.clients);
 
   if (loading) {
     return (
       <Center>
         <CircularProgress isIndeterminate color="teal.300" />
       </Center>
-    )
+    );
   }
 
   return selected.id ? (
     <>
       <ClientDetails />
-      <Divider mt={2} mb={2} />
-      <Flex>
+      <Divider m={3} />
+      <Center>
         <NewTask />
         <NewInvoice />
         <Spacer />
         <ClientInvoiceList />
         <UpdateClient />
-      </Flex>
-      <Divider mt={2} mb={2} />
+      </Center>
+      <Divider m={3} />
       <Tasks />
     </>
   ) : (
     <Overview />
-  )
+  );
 }
